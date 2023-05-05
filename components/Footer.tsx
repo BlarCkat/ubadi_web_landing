37a3bd832a567dc63dfaf456c9169c60a8b350f6
John St. Aïmond Banson <articwolf177@gmail.com>
2023-05-05 09:56:41 -0700

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+import { FaTwitter, FaFacebook, FaTiktok, FaLinkedin, FaSnapchat, FaWhatsapp, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+    return ( 
+        <>
+        <div className="h-1 w-fi"></div>
+        <div className="">
+            <span>2022&copy; Ubadi. All rights reserved.</span>
+            <div className="flex flex-row items-center gap-[16px]">
+                <FaTiktok/>
+                <FaFacebook/>
+                <FaInstagram/>
+                <FaLinkedin/>
+                <FaTwitter/>
+                <FaSnapchat/>
+                <FaWhatsapp/>
+            </div>
+        
+        </div>
+        </>
+     );
+}
+ 
+export default Footer;
